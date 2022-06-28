bc2bb395d0a4da135e8d5924699476756e18c971
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-06-29 00:08:07 +0530

    task completed

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 const listOfProducts = [
-	{
-		productName: "TV",
-		quantity: 10,
-		description: "television",
-	},
-	{
-		productName: "AC",
-		quantity: 5,
-		description: "air conditioner",
-	},
-	{
-		productName: "TV",
-		quantity: 10,
-		description: "television",
-	},
-	{
-		productName: "AC",
-		quantity: 5,
-		description: "air conditioner",
-	},
-	{
-		productName: "FAN",
-		quantity: 10,
-		description: "Ceiling Fan",
-	},
+    {
+        productName: "TV",
+        quantity: 10,
+        description: "television",
+    },
+    {
+        productName: "AC",
+        quantity: 5,
+        description: "air conditioner",
+    },
+    {
+        productName: "TV",
+        quantity: 10,
+        description: "television",
+    },
+    {
+        productName: "AC",
+        quantity: 5,
+        description: "air conditioner",
+    },
+    {
+        productName: "FAN",
+        quantity: 10,
+        description: "Ceiling Fan",
+    },
 ];
 
 
-function uniqCount(listOfProducts){
-	const obj1 = {
+function uniqCount(listOfProducts) {
+    const obj1 = {
 
     };
 
-	listOfProducts.forEach((el) => {
-		obj1[el.productName] = (obj1[el.productName] || 0) + 1;
-	});
-	console.log(obj1);
+    listOfProducts.forEach((el) => {
+        obj1[el.productName] = (obj1[el.productName] || 0) + 1;
+    });
+    console.log(obj1);
 };
 uniqCount(listOfProducts);
 
-function uniqProducts(listOfProducts){
-	const obj2 = {
-        
+function uniqProducts(listOfProducts) {
+    const obj2 = {
+
     };
-    
-	listOfProducts.forEach((el) => {
-		if (obj2[el.productName] == null){
+
+    listOfProducts.forEach((el) => {
+        if (obj2[el.productName] == null) {
             obj2[el.productName] = el;
-        }else{
+        } else {
             obj2[el.productName].quantity += el.quantity;
         }
-	});
-	console.log(obj2);
+    });
+    console.log(obj2);
 };
 
 uniqProducts(listOfProducts);
